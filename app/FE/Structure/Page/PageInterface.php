a637fa12c3d3f38a8a59fe175b00cf7202f93d83
dimon dominator <vilya04@gmail.com>
2022-01-16 00:46:58 +0200

    FE Player model

--- app/FE/Structure/Page/PageInterface.php
+++ app/FE/Structure/Page/PageInterface.php
@@ -3,18 +3,15 @@
 namespace FAFI\FE\Structure\Page;
 
 use FAFI\FE\Structure\ContentableInterface;
-use FAFI\FE\Structure\PageSection\BodyInterface;
-use FAFI\FE\Structure\PageSection\FooterInterface;
-use FAFI\FE\Structure\PageSection\HeaderInterface;
-use FAFI\FE\Structure\PageSection\TitleInterface;
+use FAFI\FE\Structure\PageSection\PageSectionInterface;
 
 interface PageInterface extends ContentableInterface
 {
     public function getTabsList(): array;
     public function initTab(string $tabName): self;
 
-    public function getHeader(): HeaderInterface;
-    public function getTitle(): TitleInterface;
-    public function getBody(): BodyInterface;
-    public function getFooter(): FooterInterface;
+    public function getHeader(): PageSectionInterface;
+    public function getTitle(): PageSectionInterface;
+    public function getBody(): PageSectionInterface;
+    public function getFooter(): PageSectionInterface;
 }
