3a6f0505925e15ab3e6ad04830f526adbe5d42d8
dimon dominator <vilya04@gmail.com>
2022-02-07 23:42:09 +0200

    added PHPUnit 9

---
+++ tests/integration/BaseIntegrationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\integration;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseIntegrationTest extends TestCase
+{
+    public function testQwerty1()
+    {
+        $this->assertTrue(true);
+    }
+}
