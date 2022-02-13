476468564338af951f991ee69c8a4c936438fdc3
dimon dominator <vilya04@gmail.com>
2022-02-13 19:18:19 +0200

    BE Country

---
+++ app/entity/GeoCountry/Repository/CountryHydrator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace FAFI\entity\GeoCountry\Repository;
+
+use FAFI\entity\GeoCountry\Country;
+use FAFI\exception\FafiException;
+
+class CountryHydrator
+{
+    private array $requiredFields = [
+        CountryResource::NAME_FIELD,
+    ];
+
+
+    /**
+     * @param array $data
+     *
+     * @return Country[]
+     * @throws FafiException
+     */
+    public function hydrateCollection(array $data): array
+    {
+        $transformed = [];
+        foreach ($data as $row) {
+            $entity = $this->hydrate($row);
+            $transformed[] = $entity;
+        }
+
+        return $transformed;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return Country
+     * @throws FafiException
+     */
+    public function hydrate(array $data): Country
+    {
+        $this->validateRequiredFieldsOnHydration($data);
+
+        return new Country(
+            isset($data[CountryResource::ID_FIELD]) ? (int)$data[CountryResource::ID_FIELD] : null,
+
+            $data[CountryResource::NAME_FIELD],
+        );
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return void
+     * @throws FafiException
+     */
+    private function validateRequiredFieldsOnHydration(array $data): void
+    {
+        $missed = [];
+        foreach ($this->requiredFields as $field) {
+            if (!isset($data[$field])) {
+                $missed[] = $field;
+            }
+        }
+
+        if (!empty($missed)) {
+            $e = sprintf(FafiException::E_REQ_MISSED, Country::ENTITY, implode('", "', $missed));
+            throw new FafiException($e);
+        }
+    }
+
+    public function extract(Country $entity): array
+    {
+        return [
+            CountryResource::ID_FIELD => $entity->getId(),
+
+            CountryResource::NAME_FIELD => $entity->getName(),
+        ];
+    }
+}
---
+++ app/entity/GeoCountry/Repository/CountryResource.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace FAFI\entity\GeoCountry\Repository;
+
+use FAFI\entity\GeoCountry\Country;
+use FAFI\entity\Structure\Repository\AbstractResource;
+use FAFI\exception\FafiException;
+
+class CountryResource extends AbstractResource
+{
+    public const TABLE = 'countries';
+    public const COLUMNS = [
+        self::ID_FIELD,
+
+        self::NAME_FIELD,
+    ];
+
+
+    public const NAME_FIELD = 'name';
+
+
+    private CountryHydrator $hydrator;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->hydrator = new CountryHydrator();
+    }
+
+
+    /**
+     * @param Country $entity
+     *
+     * @return Country
+     * @throws FafiException
+     */
+    public function create(Country $entity): Country
+    {
+        if ($entity->getId()) {
+            throw new FafiException(sprintf(self::E_ID_PRESENT, Country::ENTITY));
+        }
+
+        $data = $this->hydrator->extract($entity);
+        $id = $this->insertRecord(self::TABLE, $data);
+
+        $criteria = new CountryCriteria([$id]);
+        $result = $this->readFirst($criteria);
+        if (!$result) {
+            throw new FafiException(sprintf(self::E_ENTITY_ABSENT, Country::ENTITY, $id));
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param CountryCriteria $criteria
+     *
+     * @return Country[]|null
+     * @throws FafiException
+     */
+    public function read(CountryCriteria $criteria): ?array
+    {
+        $result = [];
+        foreach ($this->selectRecords(self::TABLE, $criteria) as $record) {
+            $result[] = $this->hydrator->hydrate($record);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param CountryCriteria $criteria
+     *
+     * @return Country|null
+     * @throws FafiException
+     */
+    public function readFirst(CountryCriteria $criteria): ?Country
+    {
+        $result = $this->selectRecords(self::TABLE, $criteria);
+        return (!empty($result)) ? $this->hydrator->hydrate($result[0]) : null;
+    }
+
+    /**
+     * @param Country $entity
+     *
+     * @return Country
+     * @throws FafiException
+     */
+    public function update(Country $entity): Country
+    {
+        if (!$entity->getId()) {
+            throw new FafiException(self::E_ID_ABSENT, Country::ENTITY);
+        }
+        $id = $entity->getId();
+
+        $data = $this->hydrator->extract($entity);
+        $this->updateRecord(self::TABLE, $data, new CountryCriteria([$id]));
+
+        $criteria = new CountryCriteria([$id]);
+        $result = $this->readFirst($criteria);
+        if (!$result) {
+            throw new FafiException(sprintf(self::E_ENTITY_ABSENT, Country::ENTITY, $id));
+        }
+
+        return $result;
+    }
+}
