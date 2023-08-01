8618aa3c39cced88e4b35991ff7341eaa4b23175
dimon dominator <vilya04@gmail.com>
2023-08-02 01:43:28 +0300

    bootstrap refactoring

--- app/boot/bootstrap.php
+++ app/boot/bootstrap.php
@@ -1,20 +1,29 @@
 <?php
 
 
-// preset system settings
-set_time_limit(0);
-ini_set('display_errors', true);
+presetSystemSettings();
+defineConstants();
+registerVendor();
 
 
-// define constants
-define('DS', DIRECTORY_SEPARATOR);
-define('EOL', PHP_EOL);
+function presetSystemSettings(): void
+{
+    set_time_limit(0);
+    ini_set('display_errors', true);
+}
 
-define('PATH_APP', dirname(realpath((__DIR__))) . DS);
-define('PATH_STORAGE', PATH_APP . 'data' . DS);
-define('PATH_QA', dirname(PATH_APP) . DS . 'tests' . DS);
-define('PATH_VENDOR', dirname(PATH_APP) . DS . 'vendor' . DS);
+function defineConstants(): void
+{
+    define('DS', DIRECTORY_SEPARATOR);
+    define('EOL', PHP_EOL);
 
+    define('PATH_APP', dirname(realpath((__DIR__))) . DS);
+    define('PATH_STORAGE', PATH_APP . 'data' . DS);
+    define('PATH_QA', dirname(PATH_APP) . DS . 'tests' . DS);
+    define('PATH_VENDOR', dirname(PATH_APP) . DS . 'vendor' . DS);
+}
 
-// register vendor classes
-require_once(PATH_VENDOR . 'autoload.php');
+function registerVendor(): void
+{
+    require_once(PATH_VENDOR . 'autoload.php');
+}
