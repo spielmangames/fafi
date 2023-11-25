e3cb7af201154ba142a9df458bd0b4815a5e9f4b
dimon dominator <vilya04@gmail.com>
2023-11-25 19:24:47 +0200

    decorations

--- app/src/BE/ImEx/ImExableEntities.php
+++ app/src/BE/ImEx/ImExableEntities.php
@@ -6,11 +6,14 @@ namespace FAFI\src\BE\ImEx;
 
 interface ImExableEntities
 {
+    // geo
     public const COUNTRIES = 'countries';
     public const CITIES = 'cities';
 
+    // team
     public const CLUBS = 'clubs';
 
+    // player
     public const POSITIONS = 'positions';
     public const PLAYERS = 'players';
     public const PLAYER_ATTRIBUTES = 'player_attributes';
