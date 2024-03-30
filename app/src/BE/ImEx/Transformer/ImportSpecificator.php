32f9118f33d9c3cf2c9baf8d9a0ab39f6dfc5e94
dimon dominator <vilya04@gmail.com>
2024-03-30 23:08:34 +0200

    Config Retriever

--- app/src/BE/ImEx/Transformer/ImportSpecificator.php
+++ app/src/BE/ImEx/Transformer/ImportSpecificator.php
@@ -5,117 +5,48 @@ declare(strict_types=1);
 namespace FAFI\src\BE\ImEx\Transformer;
 
 use FAFI\exception\FafiException;
-use FAFI\exception\ImExErr;
-use FAFI\src\BE\ImEx\Transformer\Specification\Entity\ImportableEntityConfig;
-use FAFI\src\BE\ImEx\Transformer\Specification\Field\FieldSpecification;
-use FAFI\src\BE\ImEx\Transformer\Specification\Field\FieldSpecificationFactory;
+use FAFI\src\BE\ImEx\Import\Entity\Config\ImportableEntityConfigRetriever;
+use FAFI\src\BE\ImEx\Import\ImportItem;
 
-class ImportSpecificator extends AbstractImportModule
+class ImportSpecificator
 {
-    private FieldSpecificationFactory $fieldSpecificationFactory;
+    private ImportableEntityConfigRetriever $entityConfigRetriever;
 
     public function __construct()
     {
-        parent::__construct();
-        $this->fieldSpecificationFactory = new FieldSpecificationFactory();
+        $this->entityConfigRetriever = new ImportableEntityConfigRetriever();
     }
 
 
     /**
-     * @param array[] $convertedRows
-     * @param ImportableEntityConfig $entityConfig
+     * @param ImportItem $item
      *
      * @return void
      * @throws FafiException
      */
-    public function validate(array $convertedRows, ImportableEntityConfig $entityConfig): void
+    public function validateEntity(ImportItem $item): void
     {
-        foreach ($convertedRows as $line => $convertedRow) {
-            $this->line = $line;
-            $this->validateEntity($convertedRow, $entityConfig);
+        $entityConfig = $item->getConfig();
+
+        foreach ($item->getConvertedContent() as $fieldName => $fieldValue) {
+            $fieldSpecification = $this->entityConfigRetriever->getFieldSpecification($entityConfig, $fieldName);
+            $fieldSpecification->validate($fieldName, $fieldValue);
         }
+
+        $this->validateSubEntities($item->getSubItems());
     }
 
-
     /**
-     * @param array $convertedRow
-     * @param ImportableEntityConfig $entityConfig
+     * @param ImportItem[] $subEntities
      *
      * @return void
      * @throws FafiException
      */
-    private function validateEntity(array $convertedRow, ImportableEntityConfig $entityConfig): void
+    private function validateSubEntities(array $subEntities): void
     {
-        try {
-            foreach ($convertedRow as $fieldName => $fieldValue) {
-                if ($this->isSubResource($fieldName, $entityConfig)) {
-                    $subResourceConfig = $this->prepareSubResourceConfig($fieldName, $entityConfig);
-                    $this->validateSubEntities($fieldValue, $subResourceConfig);
-                } else {
-                    $fieldSpecification = $this->prepareFieldSpecification($entityConfig, $fieldName);
-                    $fieldSpecification->validate($fieldName, $fieldValue);
-                }
-            }
-        } catch (FafiException $exception) {
-            $this->fail($exception->getMessage());
-        }
-    }
-
-    /**
-     * @param array[] $subEntities
-     * @param ImportableEntityConfig $subEntityConfig
-     *
-     * @return void
-     * @throws FafiException
-     */
-    private function validateSubEntities(array $subEntities, ImportableEntityConfig $subEntityConfig): void
-    {
-        foreach ($subEntities as $subEntity) {
-            $this->validateEntity($subEntity, $subEntityConfig);
-        }
-    }
-
-
-    /**
-     * @param ImportableEntityConfig $entityConfig
-     * @param string $field
-     *
-     * @return FieldSpecification
-     * @throws FafiException
-     */
-    private function prepareFieldSpecification(ImportableEntityConfig $entityConfig, string $field): FieldSpecification
-    {
-        $class = $this->getFieldSpecificationClass($entityConfig, $field);
-
-        try {
-            $specification = $this->fieldSpecificationFactory->create($class);
-        } catch (FafiException $exception) {
-            $this->fail($exception->getMessage());
-        }
-
-        return $specification;
-    }
-
-    /**
-     * @param ImportableEntityConfig $entityConfig
-     * @param string $field
-     *
-     * @return string
-     * @throws FafiException
-     */
-    private function getFieldSpecificationClass(ImportableEntityConfig $entityConfig, string $field): string
-    {
-        $entity = $entityConfig->getEntityName();
-        $fieldSpecificationsMap = $entityConfig->getFieldSpecificationsMap();
-
-        if (!isset($fieldSpecificationsMap[$field])) {
-            $this->fail(sprintf(ImExErr::IMPORT_ENTITY_FIELD_SPECIFICATION_ABSENT, $field, $entity));
-        }
-        $class = $fieldSpecificationsMap[$field];
-        if (!is_string($class)) {
-            $this->fail(sprintf(ImExErr::IMPORT_ENTITY_FIELD_SPECIFICATION_INVALID, $field, $entity));
-        }
-
-        return $class;
+        array_walk(
+            $subEntities,
+            fn(ImportItem $subEntity) => $this->validateEntity($subEntity)
+        );
     }
 }
